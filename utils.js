233314b3ada1ec649166758440e5f1ee5dd4502c
elonekhan <135060386+elonekhan@users.noreply.github.com>
2026-03-05 20:51:36 +0600

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:48:45.724Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T14:52:08.438Z
+Commit: Resolve null pointer exception in user service
